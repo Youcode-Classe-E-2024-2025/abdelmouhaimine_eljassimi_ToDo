b7d143c0efefcc538bd933e76f514fd950267ed7
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-28 19:50:11 +0100

    add the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
   theme: {
     extend: {
     },
+    colors: {
+      customGray: '#343435',
+      teal: '#06D6A0',
+      redOrange: '#EF476F',
+      yellow: '#FFD166',
+      coral: '#FF474A',
+      white: '#FFFFFF',
+    },
   },
   plugins: [],
 }
