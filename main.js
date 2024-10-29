5f583e0f781ad436f41a1a6d05cba7b830b7c910
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-29 16:19:18 +0100

    afficher une from pour remplire les information de la tach (pop-up)

---
+++ main.js
@@ -0,0 +1,38 @@
+let titre = document.getElementById('titre');
+let description = document.getElementById('description');
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
+let date = document.getElementById('date');
+let submit = document.getElementById('submit');
+
+
+
+
+// add task
+let task = document.getElementById('task');
+let taskForm = document.getElementById('taskForm');
+
+task.addEventListener('click', function(){       
+    taskForm.classList.remove("hidden");
+});
+
+// create task
+
+
+
+// clear inputs
+
+// Read
+
+//delete task
+
+
+
+// Sort tasks with date and preority 
+
+
+
+// Statistique 
+
+
