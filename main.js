33d5b7fd0cf35630aa94c1172b21833a4e8fcc6d
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-29 20:23:44 +0100

    Create an object and table of object and save in the local storage

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 let titre = document.getElementById('titre');
 let description = document.getElementById('description');
-let p1 = document.getElementById('p1');
-let p2 = document.getElementById('p2');
-let p3 = document.getElementById('p3');
+let section = document.getElementById('section');
 let date = document.getElementById('date');
 let submit = document.getElementById('submit');
 
 
 
-
-// add task
+// Open task form
 let task = document.getElementById('task');
 let taskForm = document.getElementById('taskForm');
+let exiteForm = document.getElementById('exiteForm');
 
 task.addEventListener('click', function(){       
     taskForm.classList.remove("hidden");
 });
+exiteForm.addEventListener('click', function(){       
+    taskForm.classList.add("hidden");
+});
+
+
+// add task
+let dataTask;
+if(localStorage.task != null){
+    dataTask = JSON.parse(localStorage.task);
+}else{
+    dataTask=[];
+}
+submit.onclick = function(){
+     let newTask = {
+        titre : titre.value,
+        description : description.value,
+        section : section.value,
+        date : date.value,
+     }
+
+     dataTask.push(newTask);
+     console.log( dataTask);
+
+    //  Save local storage
+     localStorage.setItem('task', JSON.stringify(dataTask));
+
+    //  hide the popup form 
+      taskForm.classList.add("hidden");
+}
+
+
+
+
+
+
 
 // create task
 
