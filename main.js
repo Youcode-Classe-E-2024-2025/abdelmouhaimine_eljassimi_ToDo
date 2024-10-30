8107012013bfae33d0df9136a8ce265f45850e59
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-30 09:13:58 +0100

    show the task in the right place based on the priority

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let titre = document.getElementById('titre');
 let description = document.getElementById('description');
-let section = document.getElementById('section');
+let priority = document.getElementById('priority');
 let date = document.getElementById('date');
 let submit = document.getElementById('submit');
-
+let status = document.getElementById('status');
 
 
 // Open task form
@@ -19,10 +19,11 @@ exiteForm.addEventListener('click', function(){
 });
 
 
-// add task
+// create task
 let dataTask;
-if(localStorage.task != null){
-    dataTask = JSON.parse(localStorage.task);
+if(localStorage.getItem('task') != null){
+    dataTask = JSON.parse(localStorage.getItem('task'));
+    
 }else{
     dataTask=[];
 }
@@ -30,27 +31,102 @@ submit.onclick = function(){
      let newTask = {
         titre : titre.value,
         description : description.value,
-        section : section.value,
+        priority : priority.value,
         date : date.value,
+        status: status.value
      }
-
+     
      dataTask.push(newTask);
-     console.log( dataTask);
-
     //  Save local storage
      localStorage.setItem('task', JSON.stringify(dataTask));
 
     //  hide the popup form 
       taskForm.classList.add("hidden");
+      //Show data when i click on submit button
+      showTask();
+}
+
+showTask();
+
+
+// affiche task
+function showTask(){
+    console.log(1);
+   
+    let todo = document.getElementById('todo');
+    let doing = document.getElementById('doing');
+    let done = document.getElementById('done');
+
+    todo.innerHTML = "";
+    doing.innerHTML = "";
+    done.innerHTML = "";
+    for(let i=0; i<dataTask.length; i++){
+         if(dataTask[i].status === 'TO DO'){
+
+            createHTML(todo , dataTask[i]);
+            
+        }else if(dataTask[i].status === 'DOING'){
+            
+          createHTML(doing,dataTask[i]);
+
+        }else if(dataTask[i].status === 'DONE'){
+          
+            createHTML(done, dataTask[i]);
+          
+        }
+            
+    }
 }
 
 
+function createHTML(placeholder , dataTask) {
+    let div = document.createElement('div');
+    if(dataTask.priority === 'P1'){
 
-
-
-
-
-// create task
+        div.innerHTML = `
+        <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
+                        <div class="flex justify-between p-2"> 
+                            <i class='bx bxs-edit text-white text-3xl'></i>
+                            <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
+                            <i class='bx bxs-trash text-coral text-3xl'></i>
+                        </div>
+                        <div class="flex justify-between p-2">
+                            
+                            <div class="min-w-24 h-10 bg-redOrange flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
+                            <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
+                        </div>
+                    </div>  `
+                }else if(dataTask.priority === 'P2'){
+                            div.innerHTML = `
+                            <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
+                                            <div class="flex justify-between p-2"> 
+                                                <i class='bx bxs-edit text-white text-3xl'></i>
+                                                <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
+                                                <i class='bx bxs-trash text-coral text-3xl'></i>
+                                            </div>
+                                            <div class="flex justify-between p-2">
+                                                
+                                                <div class="min-w-24 h-10 bg-yellow flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
+                                                <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
+                                            </div>
+                                        </div>  `
+                }else if(dataTask.priority === 'P3'){
+                    div.innerHTML = `
+                    <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
+                                    <div class="flex justify-between p-2"> 
+                                        <i class='bx bxs-edit text-white text-3xl'></i>
+                                        <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
+                                        <i class='bx bxs-trash text-coral text-3xl'></i>
+                                    </div>
+                                    <div class="flex justify-between p-2">
+                                        
+                                        <div class="min-w-24 h-10 bg-teal flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
+                                        <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
+                                    </div>
+                                </div>  `
+        }
+    placeholder.appendChild(div);
+}
 
 
 
