c6fef79dbeb2ae5238fcbe3caf46c8130f49e998
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2024-10-30 11:41:28 +0100

    optimizer le codepar ajouter une fonction return la couleur de background de la prioritié

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ let priority = document.getElementById('priority');
 let date = document.getElementById('date');
 let submit = document.getElementById('submit');
 let status = document.getElementById('status');
+let tacheDetails = document.getElementById('tacheDetails');
+let exiteDetails = document.getElementById('exiteDetails');
+
 
 
 // Open task form
@@ -49,10 +52,10 @@ submit.onclick = function(){
 showTask();
 
 
+
 // affiche task
 function showTask(){
-    console.log(1);
-   
+
     let todo = document.getElementById('todo');
     let doing = document.getElementById('doing');
     let done = document.getElementById('done');
@@ -78,61 +81,75 @@ function showTask(){
     }
 }
 
+// define the background color of thr priority
 
-function createHTML(placeholder , dataTask) {
+function getPriorityColor(priority) {
+    switch (priority) {
+        case 'P1': return 'bg-redOrange';
+        case 'P2': return 'bg-yellow';
+        case 'P3': return 'bg-teal';
+    } 
+}
+
+
+function createHTML(placeholder , dataTask){
     let div = document.createElement('div');
-    if(dataTask.priority === 'P1'){
 
-        div.innerHTML = `
-        <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
-                        <div class="flex justify-between p-2"> 
-                            <i class='bx bxs-edit text-white text-3xl'></i>
-                            <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
-                            <i class='bx bxs-trash text-coral text-3xl'></i>
-                        </div>
-                        <div class="flex justify-between p-2">
-                            
-                            <div class="min-w-24 h-10 bg-redOrange flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
-                            <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
-                        </div>
-                    </div>  `
-                }else if(dataTask.priority === 'P2'){
-                            div.innerHTML = `
-                            <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
-                                            <div class="flex justify-between p-2"> 
-                                                <i class='bx bxs-edit text-white text-3xl'></i>
-                                                <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
-                                                <i class='bx bxs-trash text-coral text-3xl'></i>
-                                            </div>
-                                            <div class="flex justify-between p-2">
-                                                
-                                                <div class="min-w-24 h-10 bg-yellow flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
-                                                <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
-                                            </div>
-                                        </div>  `
-                }else if(dataTask.priority === 'P3'){
-                    div.innerHTML = `
-                    <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
-                                    <div class="flex justify-between p-2"> 
-                                        <i class='bx bxs-edit text-white text-3xl'></i>
-                                        <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
-                                        <i class='bx bxs-trash text-coral text-3xl'></i>
-                                    </div>
-                                    <div class="flex justify-between p-2">
-                                        
-                                        <div class="min-w-24 h-10 bg-teal flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
-                                        <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
-                                    </div>
-                                </div>  `
-        }
+             div.innerHTML = `
+                         <div id="tache" class="bg-darkgrey w-full h-[100px] rounded-[15px] mt-4 border border-white">
+                             <div class="flex justify-between p-2"> 
+                                 <i class='bx bxs-edit text-white text-3xl'></i>
+                                 <h2 class="font-bold text-white text-3xl">${dataTask.titre}</h2>
+                                 <i class='bx bxs-trash text-coral text-3xl'></i>
+                             </div>
+                             <div class="flex justify-between p-2">
+                                 
+                                 <div class="min-w-24 h-10 ${getPriorityColor(dataTask.priority)} flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.priority}</div>
+                                 <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${dataTask.date}</div>
+                             </div>
+                         </div>  `
+
     placeholder.appendChild(div);
+
+    displayTask(dataTask, div);
+
+}
+
+function displayTask(task, taskDiv) {
+    taskDiv.onclick = function() {
+        tacheDetails.classList.remove("hidden");
+        tacheDetails.innerHTML = `
+            <div class="bg-darkgrey rounded-[40px] shadow-lg p-6 w-[90%] h-fit sm:w-[60%] md:w-[60%] lg:w-[50%] xl:w-[40%] h-fit sm:h-fit md:h-fit lg:h-fit xl:h-fit overflow-auto">
+                <div class="flex justify-between p-2"> 
+                    <i class='bx bxs-edit text-white text-3xl'></i>
+                    <h2 class="font-bold text-white text-3xl">${task.titre}</h2>
+                    <i id="exiteDetails" class='bx bxs-exit text-white text-3xl'></i>
+                </div>
+                <div class="text-center text-white p-2">
+                    ${task.description}
+                </div>
+                <div class="flex justify-between p-2">
+
+                    <div class="min-w-24 h-10 ${getPriorityColor(task.priority)} flex justify-center font-bold text-white text-1xl rounded-[4px]">${task.priority}</div>
+                    <div class="min-w-40 h-10 bg-customGray flex justify-center font-bold text-white text-1xl rounded-[4px]">${task.date}</div>
+                </div>
+            </div>
+        `;
+
+        // Close details when the exit icon is clicked
+        const exiteDetails = document.getElementById("exiteDetails");
+        exiteDetails.addEventListener('click', function() {
+            tacheDetails.classList.add("hidden");
+        });
+    }
 }
 
 
 
 // clear inputs
 
-// Read
+
+// Show task detais
 
 //delete task
 
